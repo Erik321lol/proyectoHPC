1444bd84dd8ad70d00cdac7edd155b7f7263b41e
erik guillen <guillen4473@gmail.com>
2021-04-10 02:24:18 -0600

    se agregaron: imagenes, clase contacto, estilo_contacto, se modifico el layout agregando un hipervinculo de contacto. Esta modificacion la realizo Erik

--- proyectoHPC/Controllers/HomeController.cs
+++ proyectoHPC/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace proyectoHPC.Controllers
             return View();
         }
 
+        public IActionResult contacto()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
